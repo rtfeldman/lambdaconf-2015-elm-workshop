a63bb399ebacc4883950eaa1cc58159f606b97a4
Richard Feldman <richard.t.feldman@gmail.com>
2015-05-24 05:06:14 -0600

    Update 1_basic to only do the bare minimum.

--- 1_basic/Wizardry.elm
+++ 1_basic/Wizardry.elm
@@ -31,41 +31,19 @@ import Spells exposing (Spell)
 
 -- Our complete application state.
 
-type GameStatus = Won | Lost | Playing
-
 type alias Model = {
     monsters      : List Monster,
 
     selectedSpell : Maybe Spell,
-    knownSpells   : List Spell,
-
-    playerMana    : Int,
-    playerHealth  : Int,
-
-    gameStatus    : GameStatus,
-    statusMessage : String
+    knownSpells   : List Spell
   }
 
 initialModel : Model
 initialModel = {
-    monsters      = [
-      Monsters.waterElemental,
-      Monsters.fireElemental,
-      Monsters.airElemental
-    ],
+    monsters      = [], -- TODO we should put some monsters in here.
 
     selectedSpell = Nothing,
-    knownSpells   = [
-      Spells.roast,
-      Spells.freeze,
-      Spells.zap
-    ],
-
-    playerMana    = 100,
-    playerHealth  = 100,
-
-    gameStatus    = Playing,
-    statusMessage = "Choose a spell, then choose a monster to cast it on."
+    knownSpells   = [] -- TODO we should put some spells in here.
   }
 
 
@@ -76,77 +54,14 @@ initialModel = {
 -- some alternatives: http://elm-lang.org/learn/Architecture.elm
 type Action
     = NoOp
-    | CastSpellOnMonster Int
-    | SelectSpell Spell
-    | Recharge
-
-resolveSpellOnMonster spell monster =
-  { monster | health <- monster.health - spell.damage }
-
-displayInsufficientMana model spell =
-  { model | statusMessage <- "You do not have enough mana to cast " ++ spell.name ++ "." }
 
 -- Update our Model using a given Action
 update : Action -> Model -> Model
 update action model =
-  resolveModel <| case action of
-    NoOp -> model
 
-    SelectSpell spell ->
-      { model | selectedSpell <- Just spell }
+  -- TODO this is where we should update the model based on the given action.
+  model
 
-    CastSpellOnMonster targetMonsterIndex ->
-      case model.selectedSpell of
-        Nothing ->
-          { model | statusMessage <- "Choose a spell first!" }
-
-        Just spell ->
-          if spell.manaCost > model.playerMana
-            then
-              displayInsufficientMana model spell
-
-            else
-              let updateMonsterByIndex = \index monster ->
-                if index == targetMonsterIndex
-                  then resolveSpellOnMonster spell monster
-                  else monster
-              in
-                { model |
-                    monsters      <- List.indexedMap updateMonsterByIndex model.monsters,
-                    playerMana    <- model.playerMana - spell.manaCost,
-                    statusMessage <- "Pow! You cast " ++ spell.name ++ "."
-                }
-
-    Recharge ->
-      let totalDamage = List.sum (List.map .damage model.monsters)
-      in
-        { model |
-            playerMana    <- 100,
-            playerHealth  <- model.playerHealth - totalDamage,
-            statusMessage <- "You recharge, but lose " ++ (toString totalDamage) ++ " health!"
-        }
-
--- Resolves invalid model fields as follows:
-
--- If the player has 0 health or less, the game is lost.
--- Any monsters with 0 health or less are removed.
--- If there are no monsters remaining, the game is won.
-resolveModel : Model -> Model
-resolveModel model =
-  if model.playerHealth <= 0
-    then
-      { model | gameStatus <- Lost }
-
-    else
-      let survivingMonsters =
-        List.filter (\monster -> monster.health > 0) model.monsters
-      in
-        if List.isEmpty survivingMonsters
-          then
-            { model | gameStatus <- Won }
-
-          else
-            { model | monsters <- survivingMonsters }
 
 ---- VIEW ----
 
@@ -155,60 +70,30 @@ view actions model =
   div [id "page"] [
     h1 [] [text "Elm Wizardry!"],
 
-    div [id "content"] <| case model.gameStatus of
-      Won -> [
-          div [id "you-won"] [],
-          div [class "game-status-text"] [text "YOU WON!"]
-        ]
+    -- TODO this is where we should render the spells
+    div [id "spells"] [],
 
-      Lost -> [
-          div [id "you-lost"] [],
-          div [class "game-status-text"] [text "GAME OVER!"]
-        ]
+    -- TODO this is where we should render the monsters
+    div [id "monsters"] []
+  ]
 
-      Playing -> [
-          div [id "stats"] [
-            div [id "player-health"] [text ("Health: " ++ (toString model.playerHealth))],
-            div [id "player-mana"] [text ("Mana: " ++ (toString model.playerMana))],
-            viewRecharge actions
-          ],
-
-          div [id "status-message"] [text model.statusMessage],
-
-          div [id "spells"]
-            (List.map (viewSpell actions model.selectedSpell) model.knownSpells),
-
-          div [id "monsters"]
-            (List.indexedMap (viewMonster actions) model.monsters)
-        ]
-    ]
-
-viewMonster : Address Action -> Int -> Monster -> Html
-viewMonster actions index monster =
-  div [class "monster", onClick actions (CastSpellOnMonster index)] [
+-- Render a single monster
+viewMonster : Monster -> Html
+viewMonster monster =
+  div [class "monster"] [
     div [class "monster-name"] [text monster.name],
     img [src monster.imageUrl] [],
     div [class "monster-health"] [text ((toString monster.health) ++ " ♥")],
     div [class "monster-damage"] [text ((toString monster.damage) ++ " ⚔")]
   ]
 
-viewSpell : Address Action -> Maybe Spell -> Spell -> Html
-viewSpell actions selectedSpell spell =
-  let spellClass = case selectedSpell of
-    Nothing -> "spell"
-    Just selection ->
-      if selection == spell
-        then "spell selected"
-        else "spell"
-  in
-    div [class spellClass, onClick actions (SelectSpell spell)] [
-      img [src spell.imageUrl] [],
-      span [class "spell-name"] [text spell.name]
-    ]
-
-viewRecharge : Address Action -> Html
-viewRecharge actions =
-  div [id "recharge", onClick actions Recharge] [text "Recharge Mana"]
+-- Render a single spell
+viewSpell : Spell -> Html
+viewSpell spell =
+  div [class "spell"] [
+    img [src spell.imageUrl] [],
+    span [class "spell-name"] [text spell.name]
+  ]
 
 
 ---- INPUTS ----
